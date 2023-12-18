0fe9926cf91f049dfda45f4cceb5cbc9bd84e41b
Ashutosh Verma <ashutoshrgnict@gmail.com>
2023-12-18 20:45:57 +0530

    first template

---
+++ contest.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+//SPEED OPTIMIZER
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vi=vector<int>;
+using vll=vector<ll>;
+
+void fun(){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        fun();
+    }
+}
